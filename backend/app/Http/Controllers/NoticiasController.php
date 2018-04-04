fd445c5c37071c7974930fad0937ecfdaad465cb
Alejandro Garcia <alexgarciasanz1995@gmail.com>
2018-04-04 20:55:55 +0200

    Backend call to details

--- backend/app/Http/Controllers/NoticiasController.php
+++ backend/app/Http/Controllers/NoticiasController.php
@@ -10,4 +10,8 @@ class NoticiasController extends Controller
    public function getnoticias ($category){
        return Noticias::where('category',$category)->get();
    }
+
+   public function getnoticia ($id){
+    return Noticias::where('id',$id)->get();
+}
 }
