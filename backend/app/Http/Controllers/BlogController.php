60b9434f5c30ab7ce064c97c40d3c0d30da86988
Alejandro Garcia <alexgarciasanz1995@gmail.com>
2018-05-26 01:53:10 +0200

    Filtro por categorias en blogs, SASS implementado con la maquetación actual

--- backend/app/Http/Controllers/BlogController.php
+++ backend/app/Http/Controllers/BlogController.php
@@ -83,4 +83,8 @@ class BlogController extends Controller
     public function get_entry($id){
         return Blog::where('id',$id)->get();
     }
+
+    public function filter($category){
+        return Blog::where('category',$category)->get();
+    }
 }
